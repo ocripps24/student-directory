2ee3a4668c0837493de18526e0e76cde4d2ab6ec
Oliver Cripps <ollycripps@gmail.com>
2019-03-29 15:38:02 +0000

    refactor menu code and students array

--- directory.rb
+++ directory.rb
@@ -1,27 +1,34 @@
+@students = [] # an empty array accessible to all methods
+
 def interactive_menu
-  students = []
   loop do
-    # 1. print the menu and ask the user what to do
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit"
-    # 2. read the input and save it into a variable
-    selection = gets.chomp
-    # 3. do what the user has asked
-    case selection
-      when "1"
-        # input the students
-        students = input_students
-      when "2"
-        # show the students
-        print_header
-        print(students)
-        print_footer(students)
-      when "9"
-        exit # this will cause the program to terminate
-      else
-        puts "I don't know what you meant, try again"
-      end
+    print_menu
+    process(gets.chomp)
+  end
+end
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
+
+def show_students
+  print_header
+  print_student_list
+  print_footer
+end
+
+def process(selection)
+  case selection
+    when "1"
+      input_students
+    when "2"
+      show_students
+    when "9"
+      exit
+    else
+      puts "I don't know what you meant, try again"
   end
 end
 
@@ -29,28 +36,24 @@ def input_students
   # Ask for user input
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  # Make the array to store the input
-  students = []
   # Get the first name
   name = gets.chomp
   # Until the name is empty, repeat this code
   until name.empty? do
     # Add the student hash to the array
-    students << {
+    @students << {
       name: name,
       cohort: valid_cohort?(get_cohort)
     }
-    if students.count == 1
-      puts "Now we have #{students.count} student"
+    if @students.count == 1
+      puts "Now we have #{@students.count} student"
     else
-      puts "Now we have #{students.count} students"
+      puts "Now we have #{@students.count} students"
     end
     # get another name from the user
     name = gets.chomp
   end
-  if !students.empty?
-    return students
-  else
+  if @students.empty?
     return input_students
   end
 end
@@ -80,26 +83,23 @@ def print_header
   puts "-------------"
 end
 
-def print(students)
+def print_student_list
   counter = 0
-  until counter == students.length
-    if students[counter][:name].length < 12
-      puts "#{counter + 1}. #{students[counter][:name]} (#{students[counter][:cohort]} cohort)"
-    end
+  until counter == @students.length
+    puts "#{counter + 1}. #{@students[counter][:name]} (#{@students[counter][:cohort]} cohort)"
     counter += 1
   end
 end
 
-def print_by_cohort(students)
+def print_by_cohort
   puts "which cohort to print?"
   cohort_input = gets.chomp.to_sym
-  students.map { |student| puts "#{student[:name]} (#{student[:cohort]} cohort)" if student[:cohort] == cohort_input }
+  @students.map { |student| puts "#{student[:name]} (#{student[:cohort]} cohort)" if student[:cohort] == cohort_input }
 end
 
-def print_footer(students)
-  puts "Overall, we have #{students.count} great students"
+def print_footer
+  puts "Overall, we have #{@students.count} great students"
 end
+
 # nothing happens until we call the methods
-
-
 interactive_menu
